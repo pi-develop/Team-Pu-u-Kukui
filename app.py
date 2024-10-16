52c939898225a33761146b0e176383bca4618a38
datjandra <donny.tjandra@gmail.com>
2024-10-16 13:12:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import json
 import pdfplumber
 import regex
 
-from clarifai.client.model import Model
+from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+from clarifai_grpc.grpc.api import service_pb2_grpc
+
+stub = service_pb2_grpc.V2Stub(ClarifaiChannel.get_grpc_channel())
 
 st.title("Hello World!")
