83a5158e0972d6d644c18f892cef11cf4eeae5a7
datjandra <donny.tjandra@gmail.com>
2024-10-27 14:44:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def main():
     data['BroadbandCoverage'] = data['BroadbandCoverage'].str.replace('%', '').astype(float)
 
     regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
-    m.add_geojson(regions, layer_name="US Regions")
+    # m.add_geojson(regions, layer_name="US Regions")
     m.add_points_from_xy(
         data,
         x="Latitude",
