d1832bd0033b2bffc675eb2ab9454217e107825a
datjandra <donny.tjandra@gmail.com>
2024-11-01 12:29:49 -0700

    Update device_access.py

--- pages/device_access.py
+++ pages/device_access.py
@@ -13,18 +13,10 @@ def main():
 
     df = pd.read_excel("data/acs2022_5yr_counties_hi.xlsx")
 
-    # Plot percentage of households with a computer by location
-    st.subheader("Households with a Computer (%) by Location")
-    fig_computer = px.bar(
-        df, x='Location', y='With a Computer Percent',
-        error_y='With a Computer Percent Margin of Error',
-        title="Households with a Computer by Location",
-        labels={'With a Computer Percent': 'Percentage with Computer (%)'}
-    )
-    st.plotly_chart(fig_computer)
-    
-    filtered_df = dataframe_explorer(df, case=False)
-    st.dataframe(filtered_df, use_container_width=True)
+    internet_df = pd.concat([df.iloc[0:1], df.iloc[171:175]], ignore_index=True)
+
+    filtered_df = dataframe_explorer(internet_df, case=False)
+    st.dataframe(internet_df, use_container_width=True)
     
 if __name__ == "__main__":
     main()
