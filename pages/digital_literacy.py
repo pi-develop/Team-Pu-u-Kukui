538a49678ea9ab18f23a3a0923902103d94d9d4f
datjandra <donny.tjandra@gmail.com>
2024-11-01 14:14:25 -0700

    Update digital_literacy.py

--- pages/digital_literacy.py
+++ pages/digital_literacy.py
@@ -18,7 +18,7 @@ def main():
         
         if table:
             # Convert the table into a DataFrame
-            table_df = pd.DataFrame(table[1:], columns=table[0])
+            table_df = pdf.DataFrame(table[1:], columns=table[0])
             st.write("Table extracted successfully:")
             print(table_df)
         else:
