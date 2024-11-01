36e7db5beabbedce06bf4a1f0b6aefd1ff75c7d3
datjandra <donny.tjandra@gmail.com>
2024-10-31 19:01:39 -0700

    Update style_helper.py

--- style_helper.py
+++ style_helper.py
@@ -29,7 +29,7 @@ def apply_custom_style():
       width:100%; /* Set width to 100% for responsiveness */
       height:256px;
       display: flex;
-      flex-direction: row;
+      flex-direction: column;
       justify-content: center;
       align-items: flex-start;
       padding-left: 20px;
