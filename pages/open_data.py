98b84746e40ea5918600a5dc1e627499dde2126f
datjandra <donny.tjandra@gmail.com>
2024-10-31 17:59:37 -0700

    Create open_data.py

---
+++ pages/open_data.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+from style_helper import apply_custom_style
+
+def main():
+    apply_custom_style()
+            
+    st.header("Open Data")
+
+    components.iframe("https://opendata.hawaii.gov/organization/hbdeo")
+
+if __name__ == "__main__":
+    main()
