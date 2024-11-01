7dbcdae927ea89b00d2eecd63ef57305d7f8dde6
datjandra <donny.tjandra@gmail.com>
2024-11-01 12:08:14 -0700

    Create device_access.py

---
+++ pages/device_access.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+from style_helper import apply_custom_style
+
+def main():
+    apply_custom_style()
+            
+    st.header("Device Access")
+
+if __name__ == "__main__":
+    main()
