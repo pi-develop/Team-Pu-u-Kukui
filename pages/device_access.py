0d473f6463bf00e9f0e43538b9cf775ef48c14c9
datjandra <donny.tjandra@gmail.com>
2024-11-01 12:25:53 -0700

    Update device_access.py

--- pages/device_access.py
+++ pages/device_access.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 from streamlit_extras.dataframe_explorer import dataframe_explorer
 
@@ -12,6 +13,16 @@ def main():
 
     df = pd.read_excel("data/acs2022_5yr_counties_hi.xlsx")
 
+    # Plot percentage of households with a computer by location
+    st.subheader("Households with a Computer (%) by Location")
+    fig_computer = px.bar(
+        df, x='Location', y='With a Computer Percent',
+        error_y='With a Computer Percent Margin of Error',
+        title="Households with a Computer by Location",
+        labels={'With a Computer Percent': 'Percentage with Computer (%)'}
+    )
+    st.plotly_chart(fig_computer)
+    
     filtered_df = dataframe_explorer(df, case=False)
     st.dataframe(filtered_df, use_container_width=True)
     
