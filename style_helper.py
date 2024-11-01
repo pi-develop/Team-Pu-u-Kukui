dd65a14af2a7281e7f351f58c905a2cb47736c86
datjandra <donny.tjandra@gmail.com>
2024-10-31 19:24:29 -0700

    Update style_helper.py

--- style_helper.py
+++ style_helper.py
@@ -64,8 +64,8 @@ def apply_custom_style():
     	top:57.078125px;
   }
   .header-img {
-      max-width: 281px;
-      max-height: 281px;
+      max-width: 100%;
+      max-height: 100%;
       object-fit: contain;
   }
   .stButton > button {
