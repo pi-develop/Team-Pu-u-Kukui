0feafa8254f91510bb8edd49f09d71139f763243
datjandra <donny.tjandra@gmail.com>
2024-11-01 13:01:24 -0700

    Update device_access.py

--- pages/device_access.py
+++ pages/device_access.py
@@ -31,7 +31,11 @@ def main():
               'Maui_County_Total', 'Maui_County_MOE', 'Maui_County_Percent', 'Maui_County_Percent_MOE']
 
 
+    # Extract rows for each metric
     total_households_df = internet_df[internet_df['Computers and Internet Use'] == 'Total households']
+    with_computer_df = internet_df[internet_df['Computers and Internet Use'] == 'With a computer']
+    with_broadband_df = internet_df[internet_df['Computers and Internet Use'] == 'With a broadband Internet subscription']
+    
     # Data for Total Households pie chart
     total_households = {
         'County': ['Hawaii County', 'Honolulu County', 'Kalawao County', 'Kauai County', 'Maui County'],
@@ -44,8 +48,45 @@ def main():
         ]
     }
 
-    # Plot each pie chart
-    plot_pie_chart(total_households, "Total Households by County")
+    # Data for With a Computer pie chart
+    with_computer = {
+        'County': ['Hawaii County', 'Honolulu County', 'Kalawao County', 'Kauai County', 'Maui County'],
+        'Values': [
+            with_computer_df['Hawaii_County_Total'].values[0],
+            with_computer_df['Honolulu_County_Total'].values[0],
+            with_computer_df['Kalawao_County_Total'].values[0],
+            with_computer_df['Kauai_County_Total'].values[0],
+            with_computer_df['Maui_County_Total'].values[0]
+        ]
+    }
+    
+    # Data for With Broadband Subscription pie chart
+    with_broadband = {
+        'County': ['Hawaii County', 'Honolulu County', 'Kalawao County', 'Kauai County', 'Maui County'],
+        'Values': [
+            with_broadband_df['Hawaii_County_Total'].values[0],
+            with_broadband_df['Honolulu_County_Total'].values[0],
+            with_broadband_df['Kalawao_County_Total'].values[0],
+            with_broadband_df['Kauai_County_Total'].values[0],
+            with_broadband_df['Maui_County_Total'].values[0]
+        ]
+}
+
+    # Create three columns for the pie charts
+    col1, col2, col3 = st.columns(3)
+    
+    # Plot each pie chart in its respective column
+    with col1:
+        fig1 = plot_pie_chart(total_households, "Total Households")
+        st.pyplot(fig1)
+    
+    with col2:
+        fig2 = plot_pie_chart(with_computer, "Households with a Computer")
+        st.pyplot(fig2)
+    
+    with col3:
+        fig3 = plot_pie_chart(with_broadband, "Households with Broadband Internet")
+        st.pyplot(fig3)
     
     filtered_df = dataframe_explorer(internet_df, case=False)
     st.dataframe(filtered_df, use_container_width=True)
