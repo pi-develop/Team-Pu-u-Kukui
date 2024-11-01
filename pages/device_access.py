0acc10f96d8794b98f1bdcaf4f804c8dc89c170b
datjandra <donny.tjandra@gmail.com>
2024-11-01 12:18:26 -0700

    Update device_access.py

--- pages/device_access.py
+++ pages/device_access.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+from streamlit_extras.dataframe_explorer import dataframe_explorer
+
 from style_helper import apply_custom_style
 
 def main():
@@ -10,6 +12,8 @@ def main():
 
     df = pd.read_excel("data/acs2022_5yr_counties_hi.xlsx")
 
+    filtered_df = dataframe_explorer(df, case=False)
+    st.dataframe(filtered_df, use_container_width=True)
     
 if __name__ == "__main__":
     main()
