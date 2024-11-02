c7983905738243f9ec6b788ba682e4a67adb78c4
datjandra <donny.tjandra@gmail.com>
2024-11-01 18:00:39 -0700

    Update digital_literacy.py

--- pages/digital_literacy.py
+++ pages/digital_literacy.py
@@ -13,21 +13,37 @@ def main():
     st.header("Digital Literacy")
 
     with pdfplumber.open("data/WDC Digital Literacy Report FINAL Post Client Input R111021 (003) Inc Appendices.pdf") as pdf:
-        # Select page 20 (index 19 since it's zero-indexed)
-        page = pdf.pages[20]  # Page numbers start from 0
+        # Select page 20
+        page = pdf.pages[20]
         
         # Extract the table
         table = page.extract_table()
         
         if table:
             # Convert the table into a DataFrame
-            table_df = pd.DataFrame(table[1:], columns=table[0])
-            table_df.columns = ["Category", "Unprepared", "Old Guard", "Social Users", "Technical DIYers", "Digital Learners"]
+            demographic_df = pd.DataFrame(table[1:], columns=table[0])
+            demographic_df.columns = ["Category", "Unprepared", "Old Guard", "Social Users", "Technical DIYers", "Digital Learners"]
 
-            filtered_df = dataframe_explorer(table_df, case=False)
+            filtered_df = dataframe_explorer(demographic_df, case=False)
             st.dataframe(filtered_df, use_container_width=True)
         else:
             st.write("No table found on page 20.")
+
+        # Select page 21
+        page = pdf.pages[21]
+        
+        # Extract the table
+        table = page.extract_table()
+        
+        if table:
+            # Convert the table into a DataFrame
+            occupation_df = pd.DataFrame(table[1:], columns=table[0])
+            occupation_df.columns = ["Category", "Unprepared", "Old Guard", "Social Users", "Technical DIYers", "Digital Learners"]
+
+            filtered_df = dataframe_explorer(occupation_df, case=False)
+            st.dataframe(filtered_df, use_container_width=True)
+        else:
+            st.write("No table found on page 21.")
 
 if __name__ == "__main__":
     main()
