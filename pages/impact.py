800d8e2d5057d5189a1c593861af6e521fc22a4d
datjandra <donny.tjandra@gmail.com>
2024-11-01 18:25:58 -0700

    Create impact.py

---
+++ pages/impact.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+from style_helper import apply_custom_style
+
+def main():
+    apply_custom_style()
+            
+    st.header("Initiatives Impact")
+
+if __name__ == "__main__":
+    main()
