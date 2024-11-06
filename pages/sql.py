fb4e6cdfe349f19a9bfe5437b542b46e8a8bd871
datjandra <donny.tjandra@gmail.com>
2024-11-06 12:03:28 -0800

    Update sql.py

--- pages/sql.py
+++ pages/sql.py
@@ -3,9 +3,7 @@ import streamlit as st
 #Tiger testing mysql connection11/2 
 conn = st.connection('mysql', type='sql') 
 
-st.write("Testing connection ok") 
-
 # Perform query. 
-df = conn.query('SELECT * from BroadBandConnection_By_City limit 5;', ttl=6) 
+df = conn.query('show tables', ttl=6) 
 
 st.write(df)
