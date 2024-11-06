3ef9df82d532a949d223f704032a1c6f3c356cc0
datjandra <donny.tjandra@gmail.com>
2024-11-06 12:03:59 -0800

    Update sql.py

--- pages/sql.py
+++ pages/sql.py
@@ -4,6 +4,6 @@ import streamlit as st
 conn = st.connection('mysql', type='sql') 
 
 # Perform query. 
-df = conn.query('show tables', ttl=6) 
+df = conn.query('select * from broadbcover_by_city limit 5', ttl=6) 
 
 st.write(df)
