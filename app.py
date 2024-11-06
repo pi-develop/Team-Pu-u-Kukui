3e1ef756cdb7f46190565cfdc341d501d0ebcca6
datjandra <donny.tjandra@gmail.com>
2024-11-06 13:29:23 -0800

    Update app.py

--- app.py
+++ app.py
@@ -204,8 +204,8 @@ def main():
             <div class="card-footer">
                 <span class="card-footer-text">Read more about it</span>
                 <a href="/broadband" target="_self" class="card-footer-button">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                      <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M24 12l-12-9v5h-12v8h12v5l12-9z"/>
                     </svg>
                 </a>
     """, unsafe_allow_html=True)
