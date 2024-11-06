74d450d7a8a610be495454227e24ab1eac07fdf3
datjandra <donny.tjandra@gmail.com>
2024-11-06 13:30:37 -0800

    Update app.py

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ def main():
                 <span class="card-footer-text">Read more about it</span>
                 <a href="/broadband" target="_self" class="card-footer-button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path d="M24 12l-12-9v5h-12v8h12v5l12-9z"/>
+                        <path d="M24 12l-12-9v5h-12v8h12v5l12-9z" fill="white"/>
                     </svg>
                 </a>
     """, unsafe_allow_html=True)
