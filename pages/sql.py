b9b053089b08a7f5603c81bf9d03628c3fad2f09
datjandra <donny.tjandra@gmail.com>
2024-11-06 12:04:45 -0800

    Update sql.py

--- pages/sql.py
+++ pages/sql.py
@@ -3,7 +3,8 @@ import streamlit as st
 #Tiger testing mysql connection11/2 
 conn = st.connection('mysql', type='sql') 
 
-# Perform query. 
-df = conn.query('select * from broadbcover_by_city limit 5', ttl=6) 
+df = conn.query('show tables', ttl=6) 
+st.write(df)
 
+df = conn.query('select * from broadbcover_by_city limit 5', ttl=6) 
 st.write(df)
