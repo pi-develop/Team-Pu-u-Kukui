2ac1e7d1a32a260bfde4e5952d591d1e48d567ad
datjandra <donny.tjandra@gmail.com>
2024-11-09 15:30:13 -0800

    Update digital_literacy.py

--- pages/digital_literacy.py
+++ pages/digital_literacy.py
@@ -45,19 +45,42 @@ def main():
         st.subheader("Old Guard")
         st.write(overall_row['Old_Guard'].values[0])
 
+        st.markdown("""
+        * Traditional learners with lowest tech adoption/ownership
+        * Low confidence in computer skills and new device setup
+        * Middle-aged (45-65), blue-collar/self-employed workers
+        """)
 
     with col3:
         st.subheader("Social Users")
         st.write(overall_row['Social_Users'].values[0])
+
+        st.markdown("""
+        * Digitally adept but not focused on online learning/development
+        * Strong at social networking and sharing content
+        * Young professionals (18-35), middle income, sales-oriented
+        """)
 
     col1, col2 = st.columns(2)
     with col1:
         st.subheader("Technical DIYers")
         st.write(overall_row['Technical'].values[0])
 
+        st.markdown("""
+        * Confident with tech and digital info
+        * Engages in informal online learning
+        * Single, educated professionals on O'ahu skilled in job-search tech
+        """)
+
     with col2:
         st.subheader("Digital Learners")
         st.write(overall_row['Digital'].values[0])
+
+        st.markdown("""
+        * Eager online learners; tech-confident and productive
+        * Mostly on O'ahu; educated, high-income professionals
+        * Skilled in digital creativity
+        """)
         
 if __name__ == "__main__":
     main()
