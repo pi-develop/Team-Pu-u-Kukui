cf632d78ccebdb5c65a55338effd8d3a7ecf4ff0
datjandra <donny.tjandra@gmail.com>
2024-11-11 12:45:14 -0800

    Update card_test.py

--- pages/card_test.py
+++ pages/card_test.py
@@ -78,10 +78,12 @@ with ui.card(key="card1"):
 
   # Display the custom styles in Streamlit
   st.markdown(header_style, unsafe_allow_html=True)
+
+  ui.element("div", className="card")
     
-  ui.element("span", children=["Email"], className="text-gray-400 text-sm font-medium m-1", key="label1")
-  ui.element("input", key="email_input", placeholder="Your email")
+  #ui.element("span", children=["Email"], className="text-gray-400 text-sm font-medium m-1", key="label1")
+  #ui.element("input", key="email_input", placeholder="Your email")
   
-  ui.element("span", children=["User Name"], className="text-gray-400 text-sm font-medium m-1", key="label2")
-  ui.element("input", key="username_input", placeholder="Create a User Name")
-  ui.element("button", text="Submit", key="button", className="m-1")
+  #ui.element("span", children=["User Name"], className="text-gray-400 text-sm font-medium m-1", key="label2")
+  #ui.element("input", key="username_input", placeholder="Create a User Name")
+  #ui.element("button", text="Submit", key="button", className="m-1")
