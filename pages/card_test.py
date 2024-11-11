bf4eb75d7a04a1d8c531479915fff2217744948f
datjandra <donny.tjandra@gmail.com>
2024-11-11 12:47:36 -0800

    Update card_test.py

--- pages/card_test.py
+++ pages/card_test.py
@@ -80,26 +80,26 @@ with ui.card(key="card1"):
   st.markdown(header_style, unsafe_allow_html=True)
 
   # Create a card layout with a blue header
-    st.markdown("""
-        <div class="card">
-            <div class="card-header">
-                <div>Geographical Breakdown</div>
-            </div>
-            <div>
-    """, unsafe_allow_html=True)
-
-    components.iframe("https://app.powerbi.com/view?r=eyJrIjoiM2JmM2QxZjEtYWEzZi00MDI5LThlZDMtODMzMjhkZTY2Y2Q2IiwidCI6ImMxMzZlZWMwLWZlOTItNDVlMC1iZWFlLTQ2OTg0OTczZTIzMiIsImMiOjF9", 
-                      height=500)
+  st.markdown("""
+    <div class="card">
+        <div class="card-header">
+            <div>Geographical Breakdown</div>
+        </div>
+        <div>
+  """, unsafe_allow_html=True)
     
-    # Close the card div
-    # Add the footer with "Read more about it" and a button
-    st.markdown("""
-            </div>
-            <div class="card-footer">
-                <span class="card-footer-text">Read more about it</span>
-                <a href="https://app.powerbi.com/view?r=eyJrIjoiM2JmM2QxZjEtYWEzZi00MDI5LThlZDMtODMzMjhkZTY2Y2Q2IiwidCI6ImMxMzZlZWMwLWZlOTItNDVlMC1iZWFlLTQ2OTg0OTczZTIzMiIsImMiOjF9" target="_blank" class="card-footer-button">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path d="M24 12l-12-9v5h-12v8h12v5l12-9z" fill="white"/>
-                    </svg>
-                </a>
-    """, unsafe_allow_html=True)
+  components.iframe("https://app.powerbi.com/view?r=eyJrIjoiM2JmM2QxZjEtYWEzZi00MDI5LThlZDMtODMzMjhkZTY2Y2Q2IiwidCI6ImMxMzZlZWMwLWZlOTItNDVlMC1iZWFlLTQ2OTg0OTczZTIzMiIsImMiOjF9", 
+                  height=500)
+    
+  # Close the card div
+  # Add the footer with "Read more about it" and a button
+  st.markdown("""
+        </div>
+        <div class="card-footer">
+            <span class="card-footer-text">Read more about it</span>
+            <a href="https://app.powerbi.com/view?r=eyJrIjoiM2JmM2QxZjEtYWEzZi00MDI5LThlZDMtODMzMjhkZTY2Y2Q2IiwidCI6ImMxMzZlZWMwLWZlOTItNDVlMC1iZWFlLTQ2OTg0OTczZTIzMiIsImMiOjF9" target="_blank" class="card-footer-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M24 12l-12-9v5h-12v8h12v5l12-9z" fill="white"/>
+                </svg>
+            </a>
+  """, unsafe_allow_html=True)
