530c7448a67e2085016bc2d17efdff1ceeea9e0c
datjandra <donny.tjandra@gmail.com>
2024-11-10 19:14:45 -0800

    Update sql.py

--- pages/sql.py
+++ pages/sql.py
@@ -15,7 +15,7 @@ st.write(df)
 df = conn.query('select * from use_pc_internet_by_county limit 5', ttl=6)
 st.write(df)
 
-df = conn.query('select * from readiness_by_dimensions limit 5', ttl=6)
+df = conn.query('select * from readiness_by_dimensions limit', ttl=6)
 st.write(df)
 
 df = conn.query('select * from campaign_fund limit 5', ttl=6)
