bcd12ee2c7c41270dc76f76e791e10f5b881038a
datjandra <donny.tjandra@gmail.com>
2024-11-11 21:16:33 -0800

    Update app.py

--- app.py
+++ app.py
@@ -199,7 +199,6 @@ def show_device_access_card(col):
                 label=county,
                 value=0,
                 key=f"{county}_progress").st_circular_progress()
-            county_progress.update_values(progress=90)
         
         # Close the card div
         # Add the footer with "Read more about it" and a button
