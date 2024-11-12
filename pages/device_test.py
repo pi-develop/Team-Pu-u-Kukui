b25d40040ba3386abbd63c787a2ac9a9170b210c
datjandra <donny.tjandra@gmail.com>
2024-11-12 09:36:20 -0800

    Update device_test.py

--- pages/device_test.py
+++ pages/device_test.py
@@ -1,17 +1,9 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from streamlit_extras.add_vertical_space import add_vertical_space
 
 from style_helper import apply_custom_style
-
-# Function to plot pie chart
-def plot_pie_chart(data, title):
-    fig, ax = plt.subplots()
-    ax.pie(data['Values'], labels=data['County'], autopct='%1.1f%%', startangle=90)
-    ax.set_title(title)
-    st.pyplot(fig)
 
 @st.cache_data
 def fetch_usage_data():
@@ -26,7 +18,10 @@ def main():
 
     df = fetch_usage_data()
 
-    st.dataframe(df, use_container_width=True)
+    # Filter out rows where County is 'Total households'
+    filtered_data = df[df['Use_pc_internet'] != 'Total households']
+
+    st.dataframe(filtered_data, use_container_width=True)
     
 if __name__ == "__main__":
     main()
