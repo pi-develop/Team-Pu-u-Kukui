a6268c9f2960dad739d5d2f9205e3bed704e3122
datjandra <donny.tjandra@gmail.com>
2024-11-12 10:38:47 -0800

    Update device_test.py

--- pages/device_test.py
+++ pages/device_test.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+from pygwalker.api.streamlit import StreamlitRenderer
 from streamlit_extras.add_vertical_space import add_vertical_space
-
 from style_helper import apply_custom_style
-
 from st_circular_progress import CircularProgress
 
 @st.cache_data
@@ -16,14 +15,11 @@ def fetch_usage_data():
 
 def main():
     apply_custom_style()
-            
-    st.header("Device Access")
+    st.header("Internet Usage by County")
 
     df = fetch_usage_data()
     # Filter out rows where "Use_pc_internet" is "Total households"
     df_filtered = df[df['Use_pc_internet'] != 'Total households']
-
-    st.write("Internet Usage by County")
 
     # Group by county and display each type in two columns for each county
     for county, group in df_filtered.groupby("County"):
@@ -52,7 +48,8 @@ def main():
                         key=f"cell_{i}_{col}")
                 cp.st_circular_progress()
 
-    st.dataframe(df, use_container_width=True)
+    renderer = StreamlitRenderer(df, spec="./gw_config.json")
+    renderer.explorer()
     
 if __name__ == "__main__":
     main()
