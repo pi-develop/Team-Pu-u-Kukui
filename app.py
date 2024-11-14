76307192f4dc6c7d56b9ca44754b127c6c8c8ea0
datjandra <donny.tjandra@gmail.com>
2024-11-14 12:53:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -383,7 +383,7 @@ def show_user_feedback_card(col):
                 </div>
                 <div class="card-footer">
                     <span class="card-footer-text">Read more about it</span>
-                    <a href="#" target="_self" class="card-footer-button">
+                    <a href="feedback" target="_self" class="card-footer-button">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                             <path d="M24 12l-12-9v5h-12v8h12v5l12-9z" fill="white"/>
                         </svg>
