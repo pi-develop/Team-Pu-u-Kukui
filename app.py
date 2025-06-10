6eff5d67f575eac80b534b575fae00fcd46d7768
datjandra <donny.tjandra@gmail.com>
2025-06-09 21:41:19 -1000

    Update app.py

--- app.py
+++ app.py
@@ -579,7 +579,7 @@ def show_attendance_card(col):
             </div>
             <div class="card-footer">
                 <span class="card-footer-text">Placeholder</span>
-                <a href="#" target="_self" class="card-footer-button">
+                <a href="/attendance" target="_self" class="card-footer-button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M24 12l-12-9v5h-12v8h12v5l12-9z" fill="white"/>
                     </svg>
